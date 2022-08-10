3ec9d60141e00c8cafeb8123a1ba68a9d9ee608c
nmendez4 <nmendez6594@gmail.com>
2022-08-10 15:16:35 -0400

    added and updated model folder w code

---
+++ src/main/java/VariableTypes.java
@@ -0,0 +1,5 @@
+public class VariableTypes {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/VariablesPractice.java
@@ -0,0 +1,19 @@
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 4;
+        double b = 2.69;
+        String c = "dawg";
+
+        // calculate variable sum is a double
+        double sum = a + b;
+
+        //calculated variable concat is a String
+        String concat = a + c;
+
+//        //value of sum is 6.69
+        System.out.print(sum);
+
+//        //value of concat is 4dawg
+        System.out.println(concat);
+    }
+}
